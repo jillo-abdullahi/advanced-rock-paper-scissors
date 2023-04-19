f0ea3fde4239ec297f568724621c535bd6008ca6
jillo-abdullahi <jillo.abdulla@gmail.com>
2023-04-19 13:40:28 +0300

    arrange gameboard shapes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       </Head>
 
       <main
-        className="relative h-full w-full min-h-screen"
+        className="h-full w-full min-h-screen"
         style={{
           background:
             "radial-gradient(134.34% 134.34% at 50% 0%, #1F3757 0%, #131537 100%)",
@@ -52,15 +52,76 @@ export default function Home() {
         </div>
 
         {/* // game board */}
-        <div className="flex items-center justify-center w-full">
-          <Node nodeType={NodeType.SPOCK}>
+        <div className="flex items-center justify-center w-full mt-24">
+          <div className="relative p-4 border-1 border-gray-200">
             <Image
-              src="/images/icon-lizard.svg"
-              width={62}
-              height={60}
+              className="z-50"
+              src="/images/bg-pentagon.svg"
+              width={345}
+              height={345}
               alt="rules"
             />
-          </Node>
+
+            {/* top center */}
+            <div className="absolute z-40 -top-12 left-0 right-0 mx-auto w-fit">
+              <Node nodeType={NodeType.SCISSORS}>
+                <Image
+                  src="/images/icon-scissors.svg"
+                  width={62}
+                  height={60}
+                  alt="rules"
+                />
+              </Node>
+            </div>
+
+            {/* top left  */}
+            <div className="absolute top-14 -left-12 w-fit">
+              <Node nodeType={NodeType.SPOCK}>
+                <Image
+                  src="/images/icon-spock.svg"
+                  width={62}
+                  height={60}
+                  alt="rules"
+                />
+              </Node>
+            </div>
+
+            {/* top right  */}
+            <div className="absolute top-14 -right-12 w-fit">
+              <Node nodeType={NodeType.PAPER}>
+                <Image
+                  src="/images/icon-paper.svg"
+                  width={62}
+                  height={60}
+                  alt="rules"
+                />
+              </Node>
+            </div>
+
+            {/* bottom left  */}
+            <div className="absolute -bottom-10 left-0 w-fit">
+              <Node nodeType={NodeType.LIZARD}>
+                <Image
+                  src="/images/icon-lizard.svg"
+                  width={62}
+                  height={60}
+                  alt="rules"
+                />
+              </Node>
+            </div>
+
+            {/* bottom right  */}
+            <div className="absolute -bottom-10 right-0 w-fit">
+              <Node nodeType={NodeType.ROCK}>
+                <Image
+                  src="/images/icon-rock.svg"
+                  width={62}
+                  height={60}
+                  alt="rules"
+                />
+              </Node>
+            </div>
+          </div>
         </div>
 
         {/* game rules view button  */}
